0d4989b3fdb332300ac6bb1f0869bf6496d3b670
Nikolaos Christidis <nick.christidis@glispamedia.com>
2019-05-27 09:18:58 +0300

    start of impl.

---
+++ src/main/java/com/chriniko/eresearchreponeo4jexporter/EresearchRepoNeo4jExporterApplication.java
@@ -0,0 +1,13 @@
+package com.chriniko.eresearchreponeo4jexporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EresearchRepoNeo4jExporterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EresearchRepoNeo4jExporterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chriniko/eresearchreponeo4jexporter/EresearchRepoNeo4jExporterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chriniko.eresearchreponeo4jexporter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EresearchRepoNeo4jExporterApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
